4a60211b07b8038c37f1bfe0710d5712297a4242
Hadi Haider <haiderhadi055@gmail.com>
2023-06-08 11:24:22 +0500

    feat: form API integration

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,5 +1,8 @@
 import React from "react"
 
+// axios
+import axios from "axios"
+
 // Styles
 import {
   ContactFormForm,
@@ -11,33 +14,118 @@ import {
 } from "./index.styles"
 
 const ContactForm = () => {
+  const [formData, setFormData] = React.useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    phone: "",
+    message: "",
+  })
+
+  const [formStatus, setFormStatus] = React.useState("idle") // idle, pending, success, error
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  // submit form in multipart form data
+  const handleSubmit = async e => {
+    e.preventDefault()
+    setFormStatus("pending")
+
+    const contactForm = new FormData()
+    contactForm.append("first_name", formData.first_name)
+    contactForm.append("last_name", formData.last_name)
+    contactForm.append("email", formData.email)
+    contactForm.append("phone", formData.phone)
+    contactForm.append("message", formData.message)
+
+    try {
+      const response = await axios.post(
+        "https://demoswebadalona.es//wp-json/contact-form-7/v1/contact-forms/4902/feedback",
+        contactForm,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            "Access-Control-Allow-Origin": "*",
+          },
+        }
+      )
+
+      console.log("respomns", response)
+
+      if (response.status === 200) {
+        setFormStatus("success")
+      }
+    } catch (error) {
+      console.log("error", error)
+      setFormStatus("error")
+    }
+
+    // setFormData({
+    //   first_name: "",
+    //   last_name: "",
+    //   email: "",
+    //   phone: "",
+    //   message: "",
+    // })
+
+    // setTimeout(() => {
+    //   setFormStatus("idle")
+    // }, 5000)
+  }
+
   return (
     <ContactFormStyled>
       <ContactFormWrapper>
         <ContactFormTitle>Contact Form</ContactFormTitle>
 
-        <ContactFormForm>
+        <ContactFormForm onSubmit={handleSubmit}>
           <ContactFormFormGroup>
             <div className="duo">
               <div>
                 <label htmlFor="name">First Name</label>
-                <input type="text" id="first_name" name="first_name" />
+                <input
+                  type="text"
+                  id="first_name"
+                  name="first_name"
+                  value={formData.first_name}
+                  onChange={handleChange}
+                />
               </div>
 
               <div>
                 <label htmlFor="name">Last Name</label>
-                <input type="text" id="last_name" name="last_name" />
+                <input
+                  type="text"
+                  id="last_name"
+                  name="last_name"
+                  value={formData.last_name}
+                  onChange={handleChange}
+                />
               </div>
             </div>
 
             <div className="duo">
               <div>
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" />
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
               </div>
               <div>
                 <label htmlFor="phone">Phone</label>
-                <input type="phone" id="phone" name="phone" />
+                <input
+                  type="phone"
+                  id="phone"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                />
               </div>
             </div>
 
@@ -47,6 +135,8 @@ const ContactForm = () => {
               id="message"
               cols="30"
               rows="10"
+              value={formData.message}
+              onChange={handleChange}
             ></textarea>
 
             <ContactFormFormButton type="submit">Submit</ContactFormFormButton>
